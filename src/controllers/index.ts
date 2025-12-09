d8ba53b45c4eaa8d3e420f10c92d69adbccc961c
Fadwahigga <fadwa.ali20@gmail.com>
2025-12-09 13:33:49 +0300

    Export webhook controller in index.ts to enhance modularity and facilitate handling of MoMo API callbacks.

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,7 +2,9 @@
  * Controllers barrel export
  */
 
+export * as authController from './authController';
 export * as causeController from './causeController';
 export * as donationController from './donationController';
 export * as payoutController from './payoutController';
+export * as webhookController from './webhookController';
 
